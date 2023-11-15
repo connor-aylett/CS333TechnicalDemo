c09117f7f954990e0652cbc5bd513d386de1d513
emilyseghers <emilyseghers1@gmail.com>
2023-11-14 20:34:40 -0500

    added first distance calc

--- start.py
+++ start.py
@@ -1,5 +1,7 @@
+import numpy as np
 import pandas as pd
 import csv
+import math
 
 # TODO:
 # 1. Descriptive statistics for initial dataset
@@ -67,6 +69,30 @@ rural_breakdown = dataset['Village - town'].value_counts()
 house_breakdown = dataset['House - block of flats'].value_counts()
 #print("House or Block of Flats Value Counts:" + house_breakdown.to_string())
 
+# Mapping of categorical columns to 1-5 number scales
+smoking_mapping = {'never smoked': 1, 'tried smoking': 2, 'former smoker': 4, 'current smoker': 5}
+
+alcohol_mapping = {'never': 1, 'social drinker': 3, 'drink a lot': 5}
+
+punctuality_mapping = {'i am always on time': 1, 'i am often early': 3, 'i am often running late': 5}
+
+lying_mapping = {'never': 1, 'only to avoid hurting someone': 3, 'sometimes': 4, 'everytime it suits me': 5}
+
+internet_mapping = {'less than an hour a day': 1, 'few hours a day': 3, 'most of the day': 5}
+
+gender_mapping = {'female': 0, 'male': 1}
+
+handed_mapping = {'left handed': 0, 'right handed': 1}
+
+education_mapping = {'primary school': 1, 'secondary school': 2, 'college/bachelor degree': 4, 'masters degree': 5}
+
+onlychild_mapping = {'no': 0, 'yes': 1}
+
+city_mapping = {'village': 0, 'city': 1}
+
+house_mapping = {'block of flats': 0, 'house/bungalow': 1}
+
+
 # --------------------------------------------------------------------------------------
 # -----------PART 2: K-MEANS CLUSTERING  -----------------------------------------------
 # --------------------------------------------------------------------------------------
@@ -104,3 +130,45 @@ def lloyds_kmeans(data, k, iterations):
     clusters = [finallabels[finallabels == i].index.tolist() for i in range(k)]
 
     return centroids, clusters
+
+
+def distance(user1, user2):
+    distance = 0
+
+    for x in range(len(user1)):
+        temp = 0
+        if isinstance(user1[x], np.float64):
+            temp += user1[x] - user2[x]
+        elif isinstance(user1[x], np.int64):
+            temp += user1[x] - user2[x]
+        elif user1[x] in smoking_mapping and user2[x] in smoking_mapping:
+            temp += smoking_mapping[user1[x]] - smoking_mapping[user2[x]]
+        elif user1[x] in alcohol_mapping and user2[x] in alcohol_mapping:
+            temp += alcohol_mapping[user1[x]] - alcohol_mapping[user2[x]]
+        elif user1[x] in punctuality_mapping and user2[x] in punctuality_mapping:
+            temp += punctuality_mapping[user1[x]] - punctuality_mapping[user2[x]]
+        elif user1[x] in lying_mapping and user2[x] in lying_mapping:
+            temp += lying_mapping[user1[x]] - lying_mapping[user2[x]]
+        elif user1[x] in internet_mapping and user2[x] in internet_mapping:
+            temp += internet_mapping[user1[x]] - internet_mapping[user2[x]]
+        elif user1[x] in gender_mapping and user2[x] in gender_mapping:
+            temp += gender_mapping[user1[x]] - gender_mapping[user2[x]]
+        elif user1[x] in handed_mapping and user2[x] in handed_mapping:
+            temp += handed_mapping[user1[x]] - handed_mapping[user2[x]]
+        elif user1[x] in education_mapping and user2[x] in education_mapping:
+            temp += education_mapping[user1[x]] - education_mapping[user2[x]]
+        elif user1[x] in onlychild_mapping and user2[x] in onlychild_mapping:
+            temp += onlychild_mapping[user1[x]] - onlychild_mapping[user2[x]]
+        elif user1[x] in city_mapping and user2[x] in city_mapping:
+            temp += city_mapping[user1[x]] - city_mapping[user2[x]]
+        elif user1[x] in house_mapping and user2[x] in house_mapping:
+            temp += house_mapping[user1[x]] - house_mapping[user2[x]]
+    
+        temp = temp**2
+        distance += temp
+
+    distance = math.sqrt(distance)
+
+    return distance
+
+# print(distance(dataset.iloc[0], dataset.iloc[1]))
